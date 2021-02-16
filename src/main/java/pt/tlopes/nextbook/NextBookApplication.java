149de87f8dc88c77809bc9ab27f17d4f1db74201
Tiago Lopes <tiago.lopes@criticalsoftware.com>
2021-02-16 18:22:25 +0000

    initial commit

---
+++ src/main/java/pt/tlopes/nextbook/NextBookApplication.java
@@ -0,0 +1,13 @@
+package pt.tlopes.nextbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextBookApplication.class, args);
+	}
+
+}
